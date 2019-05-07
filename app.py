81d09b38639b80b799c18c607f8bd4b1fc5b3c7a
Nick Truel <nick.truel@outlook.com>
2019-05-07 16:28:08 +0000

    Created python for sorting recipes by country, course, price and veg(or no), and linked to navbar dropdowns

--- app.py
+++ app.py
@@ -14,13 +14,82 @@ mongo = PyMongo(app)
 @app.route('/home_page')
 def home_page():
     return render_template("home.html")
-    
-@app.route('/view_recipes')
-def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+ 
+ 
+# Display recipes by country of origin  
 
+@app.route('/french_recipes')
+def french_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"recipe_country": "France"}))
+
+@app.route('/american_recipes')
+def american_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"recipe_country": "America"}))
   
+@app.route('/thai_recipes')
+def thai_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"recipe_country": "Thailand"}))
     
+@app.route('/english_recipes')
+def english_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"recipe_country": "England"}))
+    
+@app.route('/italian_recipes')
+def italian_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"recipe_country": "Italy"}))
+    
+@app.route('/indian_recipes')
+def indian_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"recipe_country": "India"}))
+  
+
+
+# Display recipes by price 
+
+@app.route('/cheap_recipes')
+def cheap_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"price_tag": "€"}))
+    
+@app.route('/medium_recipes')
+def medium_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"price_tag": "€€"}))
+
+@app.route('/expensive_recipes')
+def expensive_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"price_tag": "€€€"}))
+
+
+# Dispaly recipes by type of course
+
+@app.route('/breakfast_recipes')
+def breakfast_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"course_type": "breakfast"}))
+    
+@app.route('/starter_recipes')
+def starter_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"course_type": "starter"}))
+    
+@app.route('/main_recipes')
+def main_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"course_type": "main"}))
+
+@app.route('/desert_recipes')
+def desert_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"course_type": "desert"}))
+
+
+# Disaply recipes weither vegetarian or not
+
+@app.route('/veg_recipes')
+def veg_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"vegetarian_recipe": "yes"}))
+    
+@app.route('/non_veg_recipes')
+def non_veg_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({"vegetarian_recipe": "no"}))
+
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
